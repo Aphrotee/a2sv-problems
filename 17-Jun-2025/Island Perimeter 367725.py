0b3ea6869b26c7063056f2b29c7c323377d8171d
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-06-17 23:21:53 +0100

    Created 'Island Perimeter' by A2SV Hub

---
+++ 17-Jun-2025/Island Perimeter 367725.py
@@ -0,0 +1,17 @@
+# Problem: Island Perimeter - https://leetcode.com/problems/island-perimeter/description/
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+        return perimeter
