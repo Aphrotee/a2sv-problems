5cdb1259d69ad55918a7ec74458036264118dbb3
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-08-20 23:24:39 +0100

    Created 'Two Sum' by A2SV Hub

---
+++ 20-Aug-2025/Two Sum 387357.py
@@ -0,0 +1,11 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/description
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            else:
+                hashmap[num] = i
