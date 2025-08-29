e1ddf3044107e8b268af4a97d58219ab08d2ab2f
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-08-29 23:59:13 +0100

    Created 'Koko Eating Bananas' by A2SV Hub

---
+++ 29-Aug-2025/Koko Eating Bananas 390073.py
@@ -0,0 +1,24 @@
+# Problem: Koko Eating Bananas - https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+
+        def finish(piles, kk):
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / kk)
+            return hours
+
+        l = 1
+        r = max(piles)
+        time = r
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if finish(piles, m) <= h:
+                time = min(time, m)
+                r = m
+            else:
+                l = m + 1
+        return time
