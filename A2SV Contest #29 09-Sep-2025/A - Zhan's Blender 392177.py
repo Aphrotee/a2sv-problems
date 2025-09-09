5a53711cf037c3796b66b01fdcc8445ffc5a57f5
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-09-09 12:15:14 +0100

    Created 'A - Zhan's Blender' by A2SV Hub

---
+++ A2SV Contest #29 09-Sep-2025/A - Zhan's Blender 392177.py
@@ -0,0 +1,12 @@
+# Problem: A - Zhan's Blender - https://codeforces.com/gym/633600/problem/A
+
+import math
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        x, y = map(int, input().split())
+        print(math.ceil(n / min(x, y)))
+
+main()
