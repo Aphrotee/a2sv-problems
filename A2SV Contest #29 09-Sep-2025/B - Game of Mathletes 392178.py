a20b96a74a0a62084875b6b60d34cc3ffc23c16a
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-09-09 12:15:19 +0100

    Created 'B - Game of Mathletes' by A2SV Hub

---
+++ A2SV Contest #29 09-Sep-2025/B - Game of Mathletes 392178.py
@@ -0,0 +1,25 @@
+# Problem: B - Game of Mathletes - https://codeforces.com/gym/633600/problem/B
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int, input().split())
+        nums = list(map(int, input().split()))
+
+        nums.sort()
+        score = 0
+
+        l = 0
+        r = n - 1
+        while l < r:
+            total = nums[l] + nums[r]
+            if total == k:
+                score += 1
+                l += 1
+                r -= 1
+            elif total < k:
+                l += 1
+            elif total > k:
+                r -= 1
+        print(score)
+main()      
