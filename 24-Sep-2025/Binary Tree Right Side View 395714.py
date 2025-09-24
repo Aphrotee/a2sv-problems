c535368115f0e3ce2c84a5a6010c22f4a43933cb
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-09-24 15:16:43 +0100

    Created 'Binary Tree Right Side View' by A2SV Hub

---
+++ 24-Sep-2025/Binary Tree Right Side View 395714.py
@@ -0,0 +1,26 @@
+# Problem: Binary Tree Right Side View - https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        queue = deque([root])
+        ans = []
+        if root is None:
+            return ans
+
+        while queue:
+            levelSize = len(queue)
+            ans.append(queue[-1].val)
+            for _ in range(levelSize):
+                node = queue.popleft()
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+        return ans
+
