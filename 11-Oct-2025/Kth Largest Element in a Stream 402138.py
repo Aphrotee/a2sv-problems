b95dde9a3207fe532962830b7d801748a962d0e7
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-10-11 16:19:31 +0100

    Created 'Kth Largest Element in a Stream' by A2SV Hub

---
+++ 11-Oct-2025/Kth Largest Element in a Stream 402138.py
@@ -0,0 +1,27 @@
+# Problem: Kth Largest Element in a Stream - https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        heapq.heapify(nums)
+        self.k = k
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+        
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        output = heapq.heappop(self.heap)
+        heapq.heappush(self.heap, output)
+        return output
+        
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
