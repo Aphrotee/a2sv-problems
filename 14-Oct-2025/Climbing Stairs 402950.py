8d3bf9ab3be9e60ff008fa4e2019d3b85902f082
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2025-10-14 15:53:46 +0100

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 14-Oct-2025/Climbing Stairs 402950.py
@@ -0,0 +1,11 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+
+        for step in range(2, n + 1):
+            dp[step] = dp[step - 1] + dp[step - 2]
+        
+        return dp[n]
